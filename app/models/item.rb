93120c903303c08c37e5009b207b4945823d53be
hirokazu1209 <n.hirokazu92@gmail.com>
2021-03-27 21:48:11 +0900

    モデルの単体テストコードを書く

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,6 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-  validates :name,        presence: true
-  validates :description, presence: true
-  validates :price,       presence: true
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
@@ -14,10 +10,18 @@ class Item < ApplicationRecord
   belongs_to :shipment_source
   belongs_to :day_to_ship
 
-  validates :category_id           , numericality: { other_than: 1}
-  validates :condition_id          , numericality: { other_than: 1}
-  validates :shipping_fee_burden_id, numericality: { other_than: 1}
-  validates :shipment_source_id    , numericality: { other_than: 1}
-  validates :day_to_ship_id        , numericality: { other_than: 1}
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price, inclusion: {in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/}
+  end
 
+  with_options numericality: { other_than: 1} do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_burden_id
+    validates :shipment_source_id
+    validates :day_to_ship_id
+  end
+  validates :price, numericality: true
 end
